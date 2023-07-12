dd52aa359ad845baad03d8dd9abb9d7f3e22bd56
François Mesmacque <francois.mesmacque@sapinnov.com>
2023-07-12 13:46:34 +0000

    Basic Fiori Application template

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("umg.my.custom.library.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
