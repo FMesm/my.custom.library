f12fea2f42a8bf5a670ed854166721178f1cdc17
François Mesmacque <francois.mesmacque@sapinnov.com>
2023-07-12 13:57:07 +0000

    Reorganize files generated by the template

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["umg/my/custom/library/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"umg/my.custom.library/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
